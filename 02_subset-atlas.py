5a766a048a9e17f08b8443d59325c9ebc8e3949a
Dmitrijs Lvovs <dlvovs@smaug.igs.umaryland.edu>
2026-01-27 13:24:34 -0500

    switch to in-memory

--- 02_subset-atlas.py
+++ 02_subset-atlas.py
@@ -1,10 +1,16 @@
 import anndata as ad
 
 # read backed for efficiency
-adata = ad.read_h5ad("data/Human_HMBA_basalganglia_AIT_pre-print.h5ad", backed="r")
+print('reading atlas anndata...')
+adata = ad.read_h5ad("data/Human_HMBA_basalganglia_AIT_pre-print.h5ad")
+print('done')
 
 # subset to nucleus accumbens
-adata = adata[adata.obs["anatomical_region_merged"] == "NAC"]
+print('subsetting...')
+adata_sub = adata[adata.obs["anatomical_region_merged"] == "NAC"].copy()
+print('done')
 
 # save
-adata.write_h5ad("data/Human_HMBA_basalganglia_AIT_pre-print_NAC.h5ad")
+print('saving subset...')
+adata_sub.write_h5ad("data/Human_HMBA_basalganglia_AIT_pre-print_NAC.h5ad")
+print('all done.')
